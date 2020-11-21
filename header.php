938694d28fda07e60427a747de137d284a437df6
Pernilla Sterner <psterner@hotmail.se>
2020-11-21 18:10:48 +0100

    Add css styling to menu link when active

--- header.php
+++ header.php
@@ -15,12 +15,13 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
+          <?php $currentMenuItemClass = 'class="current-menu-item"'; ?>
           <ul>
-            <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 21) echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url( '/about-us' ); ?>">About us</a></li>
+            <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 21) echo $currentMenuItemClass; ?>><a href="<?php echo site_url( '/about-us' ); ?>">About us</a></li>
             <li><a href="<?php echo site_url('/programs'); ?>">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
+            <li <?php if(get_post_type() == 'post') echo $currentMenuItemClass; ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
           </ul>
         </nav>
         <div class="site-header__util">
