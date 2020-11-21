0cb0fcfcf56220a2fb2bf4c4f8f10de9aa861210
Pernilla Sterner <psterner@hotmail.se>
2020-11-21 20:01:45 +0100

    Add webpack for workflow automation

--- functions.php
+++ functions.php
@@ -2,13 +2,12 @@
 
 // LOADING SCRIPTS
 function add_theme_scripts() {
-  // Loading CSS
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
-  // Loading JS
-  wp_enqueue_script( 'script', get_theme_file_uri('/js/scripts-bundled.js'), null, 1.0, true);
   // Loading FONTS
   wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
   wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
+  // Loading JS & CSS
+  wp_enqueue_script( 'script', 'http://localhost:3000/bundled.js', null, 1.0, true);
+ 
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
