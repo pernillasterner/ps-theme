ece3a578e0ce1c87b13d6eb88e15ead1f122d733
Pernilla Sterner <psterner@hotmail.se>
2020-11-22 14:10:08 +0100

    Add dynamic event_date to Events section

--- front-page.php
+++ front-page.php
@@ -26,15 +26,21 @@ get_header(); ?>
       $events = new WP_Query($args);
 
       while($events->have_posts()) { $events->the_post(); ?>
-          
+      <?php $eventDate = new DateTime(get_field('event_date')); ?>
+            
       <div class="event-summary">
         <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-          <span class="event-summary__month"><?php the_time('M')?></span>
-          <span class="event-summary__day"><?php the_time('j')?></span>
+          <span class="event-summary__month"><?php echo $eventDate->format('M') ?></span>
+          <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
         </a>
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          <p><?php 
+          if(has_excerpt()) {
+            echo get_the_excerpt();
+          } else {
+            echo wp_trim_words(get_the_content(), 18);
+          }?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
         </div>
       </div>
 
@@ -67,7 +73,13 @@ get_header(); ?>
         </a>
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+          <p><?php 
+          if(has_excerpt()) {
+            echo get_the_excerpt();
+          } else {
+            echo wp_trim_words(get_the_content(), 18);
+          }?> 
+          <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
         </div>
       </div>
 
