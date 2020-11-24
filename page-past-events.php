b3957cf25df87dfb03bf11a1643b2aff19c54cd9
Pernilla Sterner <psterner@hotmail.se>
2020-11-24 14:06:04 +0100

    Add a Past Event page & add styling to nav link event

--- page-past-events.php
+++ page-past-events.php
@@ -1,22 +1,38 @@
 <?php
 /**
-* Archive Template For All The Events
+* Template For Post Type Page->Past Events
 */
 get_header(); ?>
 
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+    <h1 class="page-banner__title">Past Events</h1>
     <div class="page-banner__intro">
-      <p>See what is going on!</p>
+      <p>Recape of our pasts events!</p>
     </div>
   </div>  
 </div>
 
 <div class="container container--narrow page-section">
-<?php 
-while(have_posts()) { the_post(); ?>
+<?php
+$today = date('Ymd');
+$args = array(
+  'paged' => get_query_var('paged', 1),
+  'post_type' => 'event',
+  'meta_key' => 'event_date',
+  'orderby' => 'meta_value_num',
+  'order' => 'ASC',
+  'meta_query' => array(
+    array(
+      'key' => 'event_date',
+      'compare' => '<',
+      'value' => $today,
+      'type' => 'numeric'
+)));
+$pastEvents = new WP_Query($args);
+
+while($pastEvents->have_posts()) { $pastEvents->the_post(); ?>
   <!-- Get the event date -->
   <?php $eventDate = new DateTime(get_field('event_date')); ?>
 
@@ -31,12 +47,11 @@ while(have_posts()) { the_post(); ?>
     </div>
   </div>
 <?php }
-echo paginate_links(); 
+
+echo paginate_links(array(
+  'total' => $pastEvents->max_num_pages
+)); 
 ?>
-
-<hr class="section-break">
-
-<p>Looking for a recape of pasts events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a></p>
 </div>
 
 <?php get_footer(); ?>
