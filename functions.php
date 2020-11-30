211835b4f8129340662c1ca2c3ab9b953d5cc314
Pernilla Sterner <psterner@hotmail.se>
2020-11-30 16:39:00 +0100

    Create and display relationship between contents. Add styling to nav link

--- functions.php
+++ functions.php
@@ -22,8 +22,14 @@ add_action( 'after_setup_theme', 'init' );
 
 
 function adjust_queries($query) {
-  // Determines whether the current request is for an administrative interface page AND whether the query is the main query..
-  if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
+  // Determines whether the current request is for an administrative interface page AND whether the query is the main query.
+  if(is_admin() && is_post_type_archive('program') && is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('post_per_page', -1);
+  }
+
+  if(!is_admin() && is_post_type_archive('event') && is_main_query()) {
     $today = date('Ymd');
     $query->set('meta_key', 'event_date');
     $query->set('orderby', 'meta_value_num');
--- single-event.php
+++ single-event.php
@@ -22,11 +22,25 @@ get_header();?>
       <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home </a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
 
-    <div class="generic-content">
-      <?php the_content(); ?>
-    </div>
-  </div>
+    <div class="generic-content"><?php the_content(); ?></div>
 
+    <hr class="section-break">
+
+    <?php 
+    // Add related programs
+    $relatedPrograms = get_field('related_programs');
+    if($relatedPrograms) {
+        echo '<h2 class="headline headline--medium">Related Programs</h2>';
+        echo '<ul class="link-list min-list">';
+        foreach ($relatedPrograms as $program) { ?>
+        <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+        <?php }
+        echo '</ul>';
+    }
+    ?>
+
+  </div>
+  
 <?php } ?>
 
 <?php get_footer(); ?>
