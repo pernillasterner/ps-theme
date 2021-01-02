9ff072c3e43e3a3303d7b22e967e84add10d82ea
Pernilla Sterner <psterner@hotmail.se>
2021-01-02 12:12:47 +0100

    Add keyboard events to open/close overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -9,21 +9,36 @@ class Search
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.events();
+    this.isOverlayOpen = false; // Makes the keydown only run once
   }
 
   // 2. Events
   events() {
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
+    $(document).on('keydown', this.keyPressDispatcher.bind(this));
   }
 
   // 3. Methods / Actions
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    this.openOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
+    this.openOverlayOpen = false;
+  }
+
+  keyPressDispatcher(e) {
+    if(e.keyCode == 83 && !this.openOverlayOpen) {
+      this.openOverlay();
+    }
+    if(e.keyCode == 8 && this.openOverlayOpen) {
+      this.closeOverlay();
+    }
   }
 
 }
