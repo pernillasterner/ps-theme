15f0dfabf907773ed16e3fe148bfca24c3d0ec42
Pernilla Sterner <psterner@hotmail.se>
2021-01-02 12:34:16 +0100

    Managing typingTimer in the searchfield

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -8,8 +8,10 @@ class Search
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
-    this.isOverlayOpen = false; // Makes the keydown only run once
+    this.isOverlayOpen = false; // Makes the keydown event only run once
+    this.typingTimer;
   }
 
   // 2. Events
@@ -17,6 +19,7 @@ class Search
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
     $(document).on('keydown', this.keyPressDispatcher.bind(this));
+    this.searchField.on('keydown', this.typingLogic.bind(this));
   }
 
   // 3. Methods / Actions
@@ -39,6 +42,13 @@ class Search
     if(e.keyCode == 8 && this.openOverlayOpen) {
       this.closeOverlay();
     }
+  }
+
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function() {
+      console.log('timeout');
+    }, 2000);
   }
 
 }
